91f301434e0e86f714c7a30d9bbf522b82bbcf60
leonardovff <leonardovff@gmail.com>
2019-01-24 23:35:55 -0300

    finish drop-down height

--- src/app/components/drop-down/drop-down-list/drop-down-list.component.ts
+++ src/app/components/drop-down/drop-down-list/drop-down-list.component.ts
@@ -1,24 +1,56 @@
-import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter, HostListener, OnChanges, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-drop-down-list',
-  templateUrl: './drop-down-list.component.html',
+  template: `
+  <ul #ul *ngIf="isOpen" >
+    <li *ngFor="let row of data" (click)="clickHandle(row)">
+      {{row}}
+    </li>
+  </ul> `,
   styleUrls: ['./drop-down-list.component.scss']
 })
-export class DropDownListComponent implements OnInit {
+export class DropDownListComponent implements OnChanges, OnInit {
+  
   @Input() data: number[]; 
   @Output() selected: EventEmitter<number> = new EventEmitter();
-
+  
   @Input() isOpen: boolean;
   @Output() isOpenChange: EventEmitter<boolean> = new EventEmitter();
 
+  @ViewChild('ul') drop: ElementRef; 
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event) {
+    this.setHeight();
+  }
   constructor() { }
   clickHandle(row){
     this.selected.emit(row);
     this.isOpen = false;
     this.isOpenChange.emit(false);
   }
-  ngOnInit() {
+  ngOnInit(){
+    this.setHeight();
+  }
+  setHeight(){
+    if(this.isOpen){
+      const heightWanted = 250;
+      if(this.isOpen){
+        const top = this.drop.nativeElement.getBoundingClientRect().top;
+        const maxHeight = (top + heightWanted) > window.innerHeight ? 
+          window.innerHeight - top - 20 : heightWanted;
+        this.drop.nativeElement.parentElement.style.maxHeight = maxHeight + "px";
+      }
+    }
+  }
+  ngOnChanges() {
+    if(this.drop && !this.isOpen){ 
+      this.drop.nativeElement.parentElement.style.maxHeight = 0 + "px";
+    }
+    setTimeout(() => {
+      this.setHeight();
+    }, 50);
   }
 
 }
